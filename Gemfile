6ce7a9cea03e74970361ae75f34a0df2f20755af
Sunggyun Kim <sunggyun187@gmail.com>
2014-12-11 15:29:09 -0500

    bucket

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+ruby '2.1.2'
+
+gem 'sinatra'
+gem 'activerecord'
+
+gem 'sinatra-activerecord'
+
+gem 'pg'
---
+++ app.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+# require_relative './models/item'
+# require_relative './config/environments'
+
+#setting up the index route
+get '/' do
+  @items = Item.all
+  erb :index
+end
+
+#setting up the form view
+get '/new' do 
+  erb :new_form
+end
+
+#setting up the post route for our form
+post '/new' do
+  @item = Item.new(title: params[:title], desc: params[:desc])
+  if @item.save
+    redirect '/'
+  else
+    @errors = @item.errors.full_messages
+    render '/new'
+  end
+end
